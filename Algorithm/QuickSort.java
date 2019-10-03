c53644a11a15c4826bef99dc4985b6d507450d1c
Jeonghun-Ban <bjh5230@gmail.com>
2019-10-03 23:45:59 +0900

    Update QuickSort.md

--- Algorithm/QuickSort.java
+++ Algorithm/QuickSort.java
@@ -2,7 +2,7 @@
 import java.util.*;
 
 public class QuickSort {
-	static int i = 0;
+	public static int count = 0;
 
 	public static void main(String[] args) {
 		System.out.println("정렬할 리스트의 사이즈를 입력하세요.");
@@ -23,44 +23,29 @@ public class QuickSort {
 
     }
 
-	public static void quickSort(int[] arr, int begin, int end) {
-        if (begin < end) {
-            int p = partition(arr, begin, end); // 분할
-            quickSort(arr, begin, p - 1); // 왼쪽 부분 배열 정렬
-            quickSort(arr, p + 1, end); // 오른쪽 부분 배열 정렬
-        }
-    }
-
 	public static void swap(int [] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
-    public static int partition(int arr[], int begin, int end) {
-        int left = begin;
-        int right = end;
-        int pivot = (left + right) / 2;
+    public static void quickSort(int arr[], int begin, int end) {
+    int left = begin;					// 왼쪽 커서
+		int right = end;					// 오른쪽 커서
+		int pivot = arr[(left + right) / 2];		// 피벗
 
-        System.out.println("퀵 정렬 " + ++i + "단계: pivot: " + arr[pivot]);
+		do {
+			while (arr[left] < pivot) left++;
+			while (arr[right] > pivot) right--;
+			if (left <= right)
+				swap(arr, left++, right--);
+				System.out.print("퀵 정렬 " + ++count + "단계: ");
+				System.out.println(Arrays.toString(arr));
+		} while (left <= right);
 
-        while (left < right) {
-            while ((arr[left] < arr[pivot]) && (left < right))
-                // L 움직이는 부분
-                left++;
-            while ((arr[right] >= arr[pivot]) && (left < right))
-                // R 움직이는 부분
-                right--;
+		if (begin < right)  quickSort(arr, begin, right);
+		if (left < end) quickSort(arr, left, end);
 
-            if (left < right) {
-                swap(arr, left, right);
-            }
-        }
-
-        swap(arr, pivot, right);
-        System.out.println(Arrays.toString(arr));
-
-        return left;
     }
 
 }
